7f906198f5fc3a750b5643a973336dd56f9bb999
zuiyu1998 <1542844298@qq.com>
2024-08-29 10:23:39 +0800

    feat: 修改状态栏透明

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
+import 'dart:io';
+
 import 'package:feel/mock/index.dart';
 import 'package:feel/src/router/index.dart';
 import 'package:feel/src/utils/http/index.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
   if (kDebugMode) {
     startMockDio(defineDio.dio);
   }
 
+  await bootstrap();
+
   runApp(const MyApp());
+}
+
+Future<void> bootstrap() async {
+  //状态栏透明
+  updateStatsBar();
+}
+
+void updateStatsBar() {
+  if (Platform.isAndroid) {
+    //设置Android头部的导航栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, //全局设置透明
+    );
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
